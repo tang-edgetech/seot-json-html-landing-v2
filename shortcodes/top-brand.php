5da0205384163ec176b96a19cfbba73d5e60ff97
tsheng <tsheng@itcom.it.com>
2025-10-13 12:08:06 +0800

    Update top brand data and structure

--- shortcodes/top-brand.php
+++ shortcodes/top-brand.php
@@ -1,27 +1,30 @@
 <?php
 if( $data === 'on' || $data === 1 ) {
     $top_brands = json_decode(file_get_contents(__DIR__.'/../data/top_brands.json'), true);
-    $tb_index = 0;
+    // $tb_index = 0;
+    $top_brand_classes = "pb-0";
 ?>
-<section class="" id="">
+<section class="<?= $top_brand_classes;?>" id="">
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-12 px-3">
                 <div class="top-brand">
                 <?php 
-                $brand_slug = 'win111';
-                $brand = $top_brands[$brand_slug];
+                $first_brand = array_key_first($top_brands);
+                $brand = $top_brands[$first_brand];
                 // $brand_title = $brand['title'];
                 $brand_title = $brand['title'];
                 $brand_desc = $brand['description'];
                 // $brand_thumbnail = assets_url().'/top_brand/top_brand_'.SITE_KEY.'.png';
-                $brand_thumbnail = assets_url().'/top_brand/top_brand_'.$brand_slug.'.webp';
+                $brand_thumbnail = assets_url().'/top_brand/top_brand_'.$first_brand.'.webp';
                 $brand_link = $brand['url'];
-                $disabled = ($tb_index > 0) ? ' disabled' : '';
+                $brand_alt_text = $brand['alt'];
+                $disabled = ( isset($tb_index) && $tb_index > 0) ? ' disabled' : '';
+                $logo_alt = ( isset($brand['alt']) && !empty($brand['alt']) ? ' alt="'.$brand_alt_text.'"' : '' );
                 echo '<div class="top-brand-item '.$key.' p-2">
                     <div class="top-brand-inner d-flex align-items-center justify-content-start">
                         <div class="col col-header pt-3">
-                            <div class="col-image"><img src="'.$brand_thumbnail.'" alt="winbebas-promo"/></div>
+                            <div class="col-image"><img src="'.$brand_thumbnail.'"'.$logo_alt.'/></div>
                             <div class="col-title">'.$brand_title.'</div>
                         </div>
                         <div class="col col-desc pt-3">
