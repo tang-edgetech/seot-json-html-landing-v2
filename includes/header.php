e42633b99251898f7cabcc2817e70e1a4446419b
weiyee <edgetechnology456@gmail.com>
2026-01-20 17:04:02 +0700

    index domain

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@ $settings = json_decode(file_get_contents( __DIR__ . '/../data/settings.json' ),
 <html lang="en">
     <head>
     <?php echo get_page_meta_tags($page);?>
-    <meta name="robots" content="noindex, nofollow">
+    <meta name="robots" content="index, follow">
     <meta name="google-site-verification" content="yKaolSwxO0xvK-MjAV9NU8FGtDw_WD8GuiMgg4jAdSg">
     <base href="<?php echo home_url();?>">
     <link rel="icon" type="image/png" href="<?php echo home_url().'/assets/images/favicon/'.SITE_KEY.'_favicon.webp';?>" />
